88e899fa86c3231f87339c647f184d2f6c66e841
杨中科 <yangzhongke8@gmail.com>
2024-08-27 22:28:35 +1200

    finished basic tests

--- core/src/test/java/org/wltea/analyzer/cfg/TestUtils.java
+++ core/src/test/java/org/wltea/analyzer/cfg/TestUtils.java
@@ -1,6 +1,11 @@
 package org.wltea.analyzer.cfg;
 
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.TokenStream;
+
 import org.wltea.analyzer.dic.Dictionary;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import java.io.File;
 import java.nio.file.FileSystems;
@@ -23,6 +28,30 @@ public class TestUtils {
             hexList.add(String.format("%04x", (int) ch));
         }
         return hexList.toArray(new String[0]);
+    }
+
+    public static String[] tokenize(Configuration configuration, String s)
+    {
+        ArrayList<String> tokens = new ArrayList<>();
+        try (IKAnalyzer ikAnalyzer = new IKAnalyzer(configuration)) {
+            TokenStream tokenStream = ikAnalyzer.tokenStream("text", s);
+            tokenStream.reset();
+
+            while(tokenStream.incrementToken())
+            {
+                CharTermAttribute charTermAttribute = tokenStream.getAttribute(CharTermAttribute.class);
+                OffsetAttribute offsetAttribute = tokenStream.getAttribute(OffsetAttribute.class);
+                int len = offsetAttribute.endOffset()-offsetAttribute.startOffset();
+                char[] chars = new char[len];
+                System.arraycopy(charTermAttribute.buffer(), 0, chars, 0, len);
+                tokens.add(new String(chars));
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new RuntimeException(ex);
+        }
+        return  tokens.toArray(new String[0]);
     }
 
     /**
@@ -51,7 +80,8 @@ public class TestUtils {
 
         private static Path getConfigDir()
         {
-            return new File(System.getProperty("user.dir"), "config").toPath();
+            String projectRoot = new File(System.getProperty("user.dir")).getParentFile().getAbsolutePath();
+            return new File(projectRoot, "config").toPath();
         }
     }
 }
