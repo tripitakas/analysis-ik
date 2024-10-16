f923ccbb7b8d77e09597a9c707b403dad86ee636
杨中科 <yangzhongke8@gmail.com>
2024-10-16 21:20:50 +1300

    增加dump分词结果到CSV文件的方法

--- core/src/test/java/org/wltea/analyzer/lucene/SingleCharAnalyzerTests.java
+++ core/src/test/java/org/wltea/analyzer/lucene/SingleCharAnalyzerTests.java
@@ -1,8 +1,7 @@
 package org.wltea.analyzer.lucene;
 
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.*;
 import org.junit.Test;
 import org.wltea.analyzer.TestUtils;
 import org.wltea.analyzer.cfg.Configuration;
@@ -11,6 +10,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -56,6 +59,7 @@ public class SingleCharAnalyzerTests {
         Configuration cfg = TestUtils.createFakeConfigurationSub(false);
         String allText = readResourceText("/JS1671_025.txt");
         long countInRaw = allText.chars().filter(x -> x == '一').count();
+        dumpTokenize(cfg,allText);
         String[] values = tokenize(cfg, allText);
         long count = Arrays.stream(values).filter(x -> x.equals("一")).count();
         assert countInRaw == count;
@@ -76,6 +80,35 @@ public class SingleCharAnalyzerTests {
         assert values[3].equals("龙");
         assert values[4].equals("麟");
         assert values[5].equals("凤");
+    }
+
+    static void dumpTokenize(Configuration configuration, String s)
+    {
+        try (SingleCharAnalyzer analyzer = new SingleCharAnalyzer(configuration)) {
+            TokenStream tokenStream = analyzer.tokenStream("text", s);
+            tokenStream.reset();
+            StringBuilder sb = new StringBuilder();
+            while(tokenStream.incrementToken())
+            {
+                CharTermAttribute charTermAttribute = tokenStream.getAttribute(CharTermAttribute.class);
+                OffsetAttribute offsetAttribute = tokenStream.getAttribute(OffsetAttribute.class);
+                PositionIncrementAttribute positionIncrementAttribute = tokenStream.getAttribute(PositionIncrementAttribute.class);
+                PositionLengthAttribute positionLengthAttribute = tokenStream.getAttribute(PositionLengthAttribute.class);
+                TypeAttribute typeAttribute = tokenStream.getAttribute(TypeAttribute.class);
+                int len = offsetAttribute.endOffset()-offsetAttribute.startOffset();
+                char[] chars = new char[len];
+                System.arraycopy(charTermAttribute.buffer(), 0, chars, 0, len);
+                String line = new String(chars)+","+offsetAttribute.startOffset()+","+len+","+offsetAttribute.startOffset()+","+offsetAttribute.endOffset()+","
+                    +positionIncrementAttribute.getPositionIncrement()+","+positionLengthAttribute.getPositionLength()+
+                        ","+typeAttribute.type();
+                sb.append(line).append("\n");
+            }
+            Files.write(Paths.get("d:/dump.csv"), sb.toString().getBytes(StandardCharsets.UTF_8), new StandardOpenOption[]{StandardOpenOption.CREATE});
+        }
+        catch (Exception ex)
+        {
+            throw new RuntimeException(ex);
+        }
     }
 
     static String[] tokenize(Configuration configuration, String s)
